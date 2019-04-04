1a088c3f9c7bac76eecc2e6e0f0ca1eff12738f1
spycrab <spycrab@users.noreply.github.com>
2019-04-04 22:33:59 +0200

    server_list: Don't open a new tab when deleting sessions (closes #4)

--- admin/server_list.py
+++ admin/server_list.py
@@ -15,3 +15,19 @@ class Handler(AdminHandler):
     def view(self):
         """Return view to use"""
         return "server_list"
+
+    def admin_post(self):
+        action = self.get_argument('action', default=None)
+        secret = self.get_argument('secret', default=None)
+
+        if action != 'session_remove':
+            return
+
+        if secret is None:
+            self.set_error('Missing parameters')
+            return
+
+        if secret in api.SESSIONS:
+            del api.SESSIONS[secret]
+        if secret in api.HOSTS:
+            del api.HOSTS[secret]
