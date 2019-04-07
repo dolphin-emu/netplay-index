a78435b72eb37d360ceee26fcc320698c6b3f5fb
spycrab <spycrab@users.noreply.github.com>
2019-04-07 22:54:48 +0200

    admin/overview: Show some statistics

--- admin/overview.py
+++ admin/overview.py
@@ -2,9 +2,24 @@
 
 from admin.base import AdminHandler
 
+import api
+
 # pylint: disable=W0223
 class Handler(AdminHandler):
     """Handle admin overview"""
+
+    def template_args(self):
+        """Additional template arguments"""
+
+        total_player_count = 0
+
+        for secret in api.SESSIONS:
+            total_player_count += api.SESSIONS[secret]['player_count']
+
+        return {
+            "session_count": len(api.SESSIONS),
+            "total_player_count": total_player_count
+        }
 
     def view(self):
         """View to display"""
