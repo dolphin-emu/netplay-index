e01a661872aaf7ddfd026c3f7443161b645c0b4a
Pierre Bourdon <delroth@gmail.com>
2022-08-20 05:04:10 +0200

    nix: have poetry2nix nixpkgs follow the main flake one

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.05";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
     overlay = nixpkgs.lib.composeManyExtensions [
