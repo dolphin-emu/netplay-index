3aa4cc73aea90f22c780dd9dc4792c2e0c2a5c2d
Pierre Bourdon <delroth@gmail.com>
2022-08-20 04:34:07 +0200

    test_redirect: test behavior without requiring internet access

--- netplay_index/tests/test_redirect.py
+++ netplay_index/tests/test_redirect.py
@@ -2,11 +2,15 @@
 
 from netplay_index.tests.base import NetPlayIndexTest
 import tornado.util
+from tornado.httpclient import HTTPClientError
 from tornado.testing import gen_test
 
 
 class RedirectTest(NetPlayIndexTest):
     @gen_test
     def runTest(self):
-        response = yield self.http_client.fetch(self.get_url("/"))
-        self.assertEqual(response.effective_url, "https://dolphin-emu.org")
+        with self.assertRaises(HTTPClientError) as cm:
+            yield self.http_client.fetch(self.get_url("/"), follow_redirects=False)
+        self.assertEqual(
+            cm.exception.response.headers["Location"], "https://dolphin-emu.org"
+        )
